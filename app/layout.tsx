ae054d53ce327360841cfd196f556539d5c159f6
proteck72 <liubchenko1994@gmail.com>
2026-09-01 22:13:38 +0200

    07-routing-nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,25 @@
 import type { ReactNode } from "react";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import Providers from "./providers";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 import "./globals.css";
 
-interface RootLayoutProps {
+export default function RootLayout({
+  children,
+  modal,
+}: {
   children: ReactNode;
   modal: ReactNode;
-}
-
-export default function RootLayout({ children, modal }: RootLayoutProps) {
+}) {
   return (
     <html lang="en">
       <body>
-        <Providers>
+        <TanStackProvider>
           <Header />
           {children}
           {modal}
           <Footer />
-        </Providers>
+        </TanStackProvider>
       </body>
     </html>
   );
